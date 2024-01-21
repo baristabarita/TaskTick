f0fca61955c6e32c726aeb6aef38dded61f19918
BaristaBarita <18020776@usc.edu.ph>
2024-01-21 14:45:45 +0800

    adjusted landing page

--- client/src/pages/user/landing/LandingPage.tsx
+++ client/src/pages/user/landing/LandingPage.tsx
@@ -5,9 +5,11 @@ import Footer from '../../../components/footer/Footer';
 
 const LandingPage = () => {
     return (
-        <div>
+        <div className='flex flex-col min-h-screen'>
             <Navbar />
+            <div className='w-full mt-[6em]'>
             <h1 className='underline'>Landing Page lmao</h1>
+            </div>
             <Footer />
         </div>
     );
